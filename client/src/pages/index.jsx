be2f074ee818ed9087d6bda72a3e2b54378144ba
saikiran0701 <reddysai616@gmail.com>
2023-10-17 20:59:34 +0530

    sidebar

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export {default as Home } from './Home'
+export {default as Campaign } from './Campaign'
+export {default as CreateCampaign } from './CreateCampaign'
+export {default as Profile } from './Profile'
+
