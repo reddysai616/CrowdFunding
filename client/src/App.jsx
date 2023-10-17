1fadae6608da013c155c1aa9e3fa710072c18492
saikiran0701 <reddysai616@gmail.com>
2023-10-17 19:48:56 +0530

    assests&base src

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Route , Routes} from 'react-router-dom';
+
+ const App = () => {
+  return (
+    <div className='relative min-h-screen sm:-8 p-4 bg-[#13131a] flex flex-row text-white'>
+      App
+      <div className=' '>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
