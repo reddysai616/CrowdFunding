3b2b0bb4e5d81816fd8ba0274a411e295b3ae72d
saikiran0701 <reddysai616@gmail.com>
2023-10-20 18:03:10 +0530

    context

--- client/src/context/index.js
+++
@@ -1,9 +0,0 @@
-import { ethers } from "ethers";
-import React , {useContext , createContext} from "react";
-import {useAddress , useContract ,useMetamask , useContractWrite} from '@thirdweb-dev/react'
-
-const StateContext = createContext();
-
-export const StateContextProvider = ({children}) => {
-    
-}
