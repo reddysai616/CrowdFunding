3c1776907553d44d52c7fd1d90ce0dc6f0c2abbb
saikiran0701 <reddysai616@gmail.com>
2023-10-26 21:57:58 +0530

    profile-cards

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useStateContext } from '../context'
+import { DisplayCampaigns } from '../components';
 
 const Profile = () => {
+  const [isLoading , setIsLoading] = useState(false)
+  const [campaigns , setCampaigns] = useState([]);
+  const {address , contract , getUserCampaign} = useStateContext();
+
+  const fetchCampaign = async () => {
+    setIsLoading(true);
+   const data = await getUserCampaign();
+   setCampaigns(data);
+   setIsLoading(false);
+  }
+  useEffect(()=> {
+    if(contract)
+    fetchCampaign()
+  },[address , contract])
+
   return (
-    <div>Profile</div>
+   <DisplayCampaigns
+   title ="All Campaigns"
+   isLoading={isLoading}
+   campaigns={campaigns}
+   />
   )
 }
 
